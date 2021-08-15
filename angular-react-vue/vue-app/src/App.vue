4ffe426921bb7df87358634a8d3a04ada2fc34c5
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2021-08-15 19:36:33 +0300

    feat(vue): importing federation modules

--- angular-react-vue/vue-app/src/App.vue
+++ angular-react-vue/vue-app/src/App.vue
@@ -1,14 +1,77 @@
 <template>
-  <Header />
+  <div style="background-color: mediumseagreen; height: 94vh; padding: 16px">
+    <ng-header :title="wcTitle" @headerclick="log($event.detail)" />
+
+    <Header :title="title" @headerclick="log($event)" />
+
+    <div id="react-header"></div>
+  </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 
+// function is optional, you may init it without func
+function init() {
+  // valid imports, but if module is offline app will be dropped
+  // import { Header as ReactHeader } from 'reactApp/Header';
+  // import { HeaderComponent as AngularHeader } from 'angularApp/Header';
+  //
+  // so better use import('')
+
+  // <-- React
+  const reactPromise = Promise.all([
+    import('reactApp/Header'),
+    import('reactApp/utils'),
+  ]);
+  reactPromise.then(([{ Header: ReactHeader }, { renderReactElement }]) => {
+    renderReactElement({
+      ReactElement: ReactHeader,
+      props: {
+        title: '✅ custom title works',
+        onHeaderClick: this.log,
+      },
+      selector: '#react-header',
+    });
+  });
+  // --->
+
+  // <-- Angular
+  const angularPromise = Promise.all([
+    import('angularApp/Header'),
+    import('angularApp/utils'),
+  ]);
+  angularPromise.then(
+    ([{ HeaderComponent: AngularHeader }, { defineAngularWebComponent }]) => {
+      defineAngularWebComponent({
+        AngularComponent: AngularHeader,
+        name: 'ng-header',
+      });
+    }
+  );
+  // --->
+}
+
 export default {
   name: 'App',
   components: {
     Header,
+  },
+  data() {
+    return {
+      title: '✅ custom title works',
+      wcTitle: '🍻 Web Component title',
+    };
+  },
+  methods: {
+    log(msg) {
+      console.log(msg);
+    },
+  },
+  mounted() {
+    // init all external packages when mounted
+    // its ok to execute it on imports level
+    init.apply(this);
   },
 };
 </script>
