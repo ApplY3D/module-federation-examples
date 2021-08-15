237a1f8f3d13a6a394e9f7c4517f0e92ad14ec0e
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2021-08-15 19:36:03 +0300

    feat(react): importing federation modules

--- angular-react-vue/react-app/src/components/App.tsx
+++ angular-react-vue/react-app/src/components/App.tsx
@@ -1,10 +1,98 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { Header } from './Header';
 
+const flex1 = { flex: 1 };
+const displayFlex = { display: 'flex' };
+const wrapperStyle = {
+  backgroundColor: 'rgb(97, 218, 251)',
+  height: '94vh',
+  padding: '16px',
+};
+
+function log(msg: string) {
+  console.log(msg);
+}
+
+function logWC(event: CustomEvent) {
+  console.log(event.detail);
+}
+
 export const App = () => {
+  const title = '✅ custom title works';
+  const wcTitle = '🍻 Web Component title';
+
+  useEffect(() => {
+    async function init() {
+      try {
+        const vuePromise = Promise.all([
+          //@ts-expect-error
+          import('vueApp/Header'),
+          //@ts-expect-error
+          import('vueApp/utils'),
+        ]);
+        vuePromise.then(
+          ([
+            { default: VueHeader },
+            { renderVueElement, defineVueWebComponent },
+          ]) => {
+            const { vueApp, vueEl } = renderVueElement({
+              VueElement: VueHeader,
+              selector: '#vue-header',
+              props: { title, onHeaderclick: log },
+            });
+
+            defineVueWebComponent({
+              VueElement: VueHeader,
+              name: 'vue-header',
+            });
+          }
+        );
+
+        const angularPromise = Promise.all([
+          //@ts-expect-error
+          import('angularApp/Header'),
+          //@ts-expect-error
+          import('angularApp/utils'),
+        ]);
+        angularPromise.then(
+          ([
+            { HeaderComponent: AngularHeader },
+            { defineAngularWebComponent },
+          ]) => {
+            defineAngularWebComponent({
+              AngularComponent: AngularHeader,
+              name: 'ng-header',
+            });
+          }
+        );
+
+        const ngHeader = document.createElement('ng-header');
+        ngHeader.title = wcTitle;
+        ngHeader.addEventListener('headerclick', (e) =>
+          logWC(e as CustomEvent)
+        );
+        document.getElementById('ng-header-wc')?.appendChild(ngHeader);
+      } catch (error) {}
+    }
+
+    init();
+  }, []);
+
   return (
-    <div>
-      <Header />
+    <div style={wrapperStyle}>
+      <div style={displayFlex}>
+        <div id='ng-header-wc' style={flex1}></div>
+      </div>
+
+      <div style={displayFlex}>
+        <div id='vue-header' style={flex1}></div>
+
+        {/* @ts-expect-error */}
+        <vue-header style={flex1} title={wcTitle} />
+      </div>
+
+      <Header onHeaderClick={log} />
     </div>
   );
 };
