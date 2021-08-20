3d23edb6722d4c278a2fb4d71d9ebd6f3cf04000
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2021-08-20 14:02:56 +0300

    feat(angular): ngrx init

--- angular-ngrx-vue-router/angular-remote/src/app/app.module.ts
+++ angular-ngrx-vue-router/angular-remote/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [],
 })
-export class AppModule { }
+export class AppModule {
+  ngDoBootstrap() {
+    console.log('Bootstrapped ng app');
+  }
+}
